6b930f57766fa1a67ee2d0f0cc232a09aea80b8c
dportal-cni <dportal@centripetal.ai>
2026-01-06 23:14:51 -0500

    Fix Sanity config
    CI Build sanity fix

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,8 +1,12 @@
 import { createClient } from "@sanity/client";
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "kz27vn4i";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01";
+
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  projectId,
+  dataset,
+  apiVersion,
   useCdn: true,
 });
